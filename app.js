5464b7514b276c7f47e669872b4287c815edda3c
Sergio Celada <sergio.celada101@alu.ulpgc.es>
2020-10-25 00:25:27 +0100

    Mejoras propuestas

--- app.js
+++ app.js
@@ -10,9 +10,7 @@ var usersRouter = require('./routes/users');
 //Nuevas rutas
 var serviciosRouter = require('./routes/servicios');
 var bdRouter = require('./routes/bd');
-
-
-
+var mejorasRouter = require('./mejoras/bd');
 
 var app = express();
 
@@ -32,6 +30,7 @@ app.use('/users', usersRouter);
 //Nuevas rutas
 app.use('/servicios', serviciosRouter);
 app.use('/bd', bdRouter);
+app.use('/mejoras', mejorasRouter);
 
 
 // catch 404 and forward to error handler
